e553d32df43fb20ff271aefcd819a8bc97b0a524
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-14 19:23:51 +0100

    test

---
+++ lotto/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lotto</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h1>Lotto</h1>
+        <div class="tokens">
+            <span id="tokens">100</span> MEG-Taler
+        </div>
+    </div>
+    <div class="main">
+        <div class="lotto-tickets">
+            <!-- Hier werden die Lottoscheine angezeigt -->
+        </div>
+        <button class="buy-ticket">Neuen Schein kaufen</button>
+    </div>
+    <script src="main.js"></script>
+</body>
+</html>
