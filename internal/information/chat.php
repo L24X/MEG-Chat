7cec37520945fa3298bc7291baeb121d1058feb4
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-15 06:46:07 +0100

    Update chat.php

--- internal/information/chat.php
+++ internal/information/chat.php
@@ -91,8 +91,7 @@ if(isset($_SERVER['HTTP_USER_AGENT'])){
 			        </div>
 			    <?php } ?>
 		        <div style="float: left; width: <?php if(!$is_mobile){ ?>calc( 100% - 542px )<?php } else { ?>100%<?php } ?>; min-width: 350px; max-width: 100%; text-align: center; height: 100%; " id="chat_container">
-	            <?php if(!$chat_data){
-				    ?>
+	            <?php if(!$chat_data){ ?>
 				    <h1>Entweder dieser Chat exestiert nicht oder zu hast keinen Zugriff darauf. Sollte dieses Problem weiterhin auftauchen melde dich bitte bei einem Administrator.!</h1>
 	                <?php
 				} else { ?>
@@ -198,7 +197,7 @@ if(isset($_SERVER['HTTP_USER_AGENT'])){
 							?>
 	                    </div>
 	                </div>
-	            <?php } } ?>
+	            <?php } ?>
 		    <?php } ?>
 		</div>
     </body>
