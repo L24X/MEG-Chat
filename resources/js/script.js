51f1ed756da2392283d16f9fec6d4fd3b142d8e0
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-15 15:44:42 +0100

    Chat Nachrichten laden Ascnron gemach

--- resources/js/script.js
+++ resources/js/script.js
@@ -518,126 +518,131 @@ window.get_messages_data = async function(){
 			reset_chat();
 		}
 	}
-	function add_to_chat(data){
-		var is_first = (last_message_id == -1);
-		var first_new = true;
-		data.forEach(function(z){
-			if(Number(z.id) <= Number(last_message_id) || document.getElementById("message_"+chat_id+"_"+z.id)) return;
-			
-			if(!document.getElementById("redline")){
-			    if(z.new && first_new){
-					first_new = false;
-					if(is_first || document.hidden){
-						var r = document.createElement("div");
-						r.style = "width: 100%; height: 1px; background-color: red; margin-top: 10px; ";
-						r.id = "redline";
-						document.getElementById("chat_inner_data").insertAdjacentHTML("beforeend", r.outerHTML+"<br>");
+	if(!chat_id) return;
+	
+	return new Promise(function(resolve, reject){
+		function add_to_chat(data){
+			var is_first = (last_message_id == -1);
+			var first_new = true;
+			data.forEach(function(z){
+				if(Number(z.id) <= Number(last_message_id) || document.getElementById("message_"+chat_id+"_"+z.id)) return;
+				
+				if(!document.getElementById("redline")){
+				    if(z.new && first_new){
+						first_new = false;
+						if(is_first || document.hidden){
+							var r = document.createElement("div");
+							r.style = "width: 100%; height: 1px; background-color: red; margin-top: 10px; ";
+							r.id = "redline";
+							document.getElementById("chat_inner_data").insertAdjacentHTML("beforeend", r.outerHTML+"<br>");
+						}
 					}
+				} else if(!is_first && !document.hidden){
+					document.getElementById("redline").remove();
 				}
-			} else if(!is_first && !document.hidden){
-				document.getElementById("redline").remove();
-			}
-			
-			last_message_id = Number(z.id);
-			
-			if(last_message_author_id == z.author.id){
-				var ne = document.createElement("div");
-				ne.style = "width: 100%; height: auto; min-height: 20px; word-warp: break-word; color: white; text-align: left; font-size: 14px; position: relative; word-wrap: break-word; ";
-				ne.id = "message_"+chat_id+"_"+z.id;
-				var nei = document.createElement("div");
-				nei.style = "margin-left: 44px; ";
-				var nt = document.createElement("span");
-				nt.innerText = z.text;
-				nt.style = "word-wrap: break-word; ";
-				nt.onclick = function(){
-				    
-				};
-				nei.appendChild(nt);
-				ne.appendChild(nei);
-
-				if(!document.getElementById("chat_inner_data") || !document.getElementById("chat_inner_data_container")) return;
 				
-				document.getElementById("chat_inner_data").insertAdjacentHTML("beforeend", ne.outerHTML);
-				document.getElementById("chat_inner_data_container").scrollTop = document.getElementById("chat_inner_data_container").scrollHeight;
-			} else {
-				var ne = document.createElement("div");
-				ne.style = "width: 100%; height: auto; margin-top: 10px; min-height: 40px; word-warp: break-word; color: white; text-align: left; font-size: 14px; position: relative; transition: all 0.4s; border-radius: 12px; word-wrap: break-word; ";
-				ne.id = "message_"+chat_id+"_"+z.id;
-				var nei = document.createElement("div");
-				nei.style = "margin-left: 44px; margin-top: 4px; ";
-				var na = document.createElement("u");
-				na.innerText = z.author.username;
-				na.style = "font-weight: bold; cursor: pointer; ";
-				na.onclick = 'page_navigate("/schueler/'+z.author.id+');';
-				nei.appendChild(na);
-				var na2 = document.createElement("span");
-				na2.innerText = z.time;
-				na2.style = "font-size: 8px; font-weight: small; margin-left: 10px; ";
-				nei.appendChild(na2);
-				var nt = document.createElement("span");
-				nt.style = "margin-left: 10px; word-wrap: break-word; ";
-				nt.innerText = "\n"+z.text;
-				nei.appendChild(nt);
-				ne.appendChild(nei);
-				var neb = document.createElement("div");
-				neb.style = "position: absolute; top: 0px; left: 0px; height: 40px; width: 40px; display: flex; justify-content: center; align-items: center; ";
-				var neba = document.createElement("img");
-				neba.loading = "lazy";
-				neba.style = "width: 34px; height: 34px; border-radius: 50%; ";
-				neba.src = z.author.avatar || "/resources/images/avatar.png";
-				neb.appendChild(neba);
-				ne.appendChild(neb);
+				last_message_id = Number(z.id);
 				
-				if(!document.getElementById("chat_inner_data") || !document.getElementById("chat_inner_data_container")) return;
+				if(last_message_author_id == z.author.id){
+					var ne = document.createElement("div");
+					ne.style = "width: 100%; height: auto; min-height: 20px; word-warp: break-word; color: white; text-align: left; font-size: 14px; position: relative; word-wrap: break-word; ";
+					ne.id = "message_"+chat_id+"_"+z.id;
+					var nei = document.createElement("div");
+					nei.style = "margin-left: 44px; ";
+					var nt = document.createElement("span");
+					nt.innerText = z.text;
+					nt.style = "word-wrap: break-word; ";
+					nt.onclick = function(){
+					    
+					};
+					nei.appendChild(nt);
+					ne.appendChild(nei);
+	
+					if(!document.getElementById("chat_inner_data") || !document.getElementById("chat_inner_data_container")) return;
+					
+					document.getElementById("chat_inner_data").insertAdjacentHTML("beforeend", ne.outerHTML);
+					document.getElementById("chat_inner_data_container").scrollTop = document.getElementById("chat_inner_data_container").scrollHeight;
+				} else {
+					var ne = document.createElement("div");
+					ne.style = "width: 100%; height: auto; margin-top: 10px; min-height: 40px; word-warp: break-word; color: white; text-align: left; font-size: 14px; position: relative; transition: all 0.4s; border-radius: 12px; word-wrap: break-word; ";
+					ne.id = "message_"+chat_id+"_"+z.id;
+					var nei = document.createElement("div");
+					nei.style = "margin-left: 44px; margin-top: 4px; ";
+					var na = document.createElement("u");
+					na.innerText = z.author.username;
+					na.style = "font-weight: bold; cursor: pointer; ";
+					na.onclick = 'page_navigate("/schueler/'+z.author.id+');';
+					nei.appendChild(na);
+					var na2 = document.createElement("span");
+					na2.innerText = z.time;
+					na2.style = "font-size: 8px; font-weight: small; margin-left: 10px; ";
+					nei.appendChild(na2);
+					var nt = document.createElement("span");
+					nt.style = "margin-left: 10px; word-wrap: break-word; ";
+					nt.innerText = "\n"+z.text;
+					nei.appendChild(nt);
+					ne.appendChild(nei);
+					var neb = document.createElement("div");
+					neb.style = "position: absolute; top: 0px; left: 0px; height: 40px; width: 40px; display: flex; justify-content: center; align-items: center; ";
+					var neba = document.createElement("img");
+					neba.loading = "lazy";
+					neba.style = "width: 34px; height: 34px; border-radius: 50%; ";
+					neba.src = z.author.avatar || "/resources/images/avatar.png";
+					neb.appendChild(neba);
+					ne.appendChild(neb);
+					
+					if(!document.getElementById("chat_inner_data") || !document.getElementById("chat_inner_data_container")) return;
+					
+					document.getElementById("chat_inner_data").insertAdjacentHTML("beforeend", ne.outerHTML);
+					document.getElementById("chat_inner_data_container").scrollTop = document.getElementById("chat_inner_data_container").scrollHeight;
+				}
 				
-				document.getElementById("chat_inner_data").insertAdjacentHTML("beforeend", ne.outerHTML);
-				document.getElementById("chat_inner_data_container").scrollTop = document.getElementById("chat_inner_data_container").scrollHeight;
-			}
-			
-			last_message_author_id = z.author.id;
-			loaded_messages_count++;
-			
-			var messages_count = Number(document.getElementById("chat_messages_count").innerText);
-			if(loaded_messages_count > messages_count){
-			    messages_count++;
-			    document.getElementById("chat_messages_count").innerText = messages_count;
-			}
-			
-			try {
-				z.new = false;
-				var o = JSON.parse(localStorage.getItem("chat_"+chat_id) || "[]");
-				o.push(z);
-				localStorage.setItem("chat_"+chat_id, JSON.stringify(o));
-			} catch(e){
-			    console.log(e);	
-			}
-		});
-	}
-	try {
-		var has = [];
-		var o = JSON.parse(localStorage.getItem("chat_"+chat_id) || "[]");
-		o.forEach(function(z){
-		    if(z.id > last_message_id) has.push(z);	
-		});
-		if(has.length > 0){
-			add_to_chat(has);
+				last_message_author_id = z.author.id;
+				loaded_messages_count++;
+				
+				var messages_count = Number(document.getElementById("chat_messages_count").innerText);
+				if(loaded_messages_count > messages_count){
+				    messages_count++;
+				    document.getElementById("chat_messages_count").innerText = messages_count;
+				}
+				
+				try {
+					z.new = false;
+					var o = JSON.parse(localStorage.getItem("chat_"+chat_id) || "[]");
+					o.push(z);
+					localStorage.setItem("chat_"+chat_id, JSON.stringify(o));
+				} catch(e){
+				    console.log(e);	
+				}
+			});
 		}
-	} catch(e){
-	    console.log(e);	
-	}
-	if(!("running_chat_reader" in window)) {
-		window.running_chat_reader = false;
-	}
-	if(running_chat_reader) return;
-	window.running_chat_reader = true;
-	post_request("/ajax/load_new_messages.php", {chat: chat_id, last: Number(last_message_id)}, function(data){
-		setTimeout(function(){
+		try {
+			var has = [];
+			var o = JSON.parse(localStorage.getItem("chat_"+chat_id) || "[]");
+			o.forEach(function(z){
+			    if(z.id > last_message_id) has.push(z);	
+			});
+			if(has.length > 0){
+				add_to_chat(has);
+			}
+		} catch(e){
+		    console.log(e);	
+		}
+		if(!("running_chat_reader" in window)) {
 			window.running_chat_reader = false;
-		    get_messages_data();
-		}, 50);
-		data = JSON.parse(data);
-		add_to_chat(data);
-	});
+		}
+		if(running_chat_reader) return;
+		window.running_chat_reader = true;
+		post_request("/ajax/load_new_messages.php", {chat: chat_id, last: Number(last_message_id)}, function(data){
+			setTimeout(function(){
+				window.running_chat_reader = false;
+			    get_messages_data();
+			}, 50);
+			data = JSON.parse(data);
+			add_to_chat(data);
+			resolve();
+		});
+    });
 }
 get_messages_data();
 addLoadEvent(get_messages_data);
