f4c58621fa47bba6737bd5fbe222d0bade0c9cb5
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-18 22:38:54 +0100

    Update connect.php

--- ajax/open/connect.php
+++ ajax/open/connect.php
@@ -1,7 +1,7 @@
 <?php
-require_once("../../internal/logic/db.php");
-
 header("Access-Control-Allow-Origin: *");
+
+require_once("../../internal/logic/db.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	if(!isset($_POST['data'])) return;
