c001efcfa8f5ab438d3263906b12c2241457062a
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-18 22:29:12 +0100

    Api

--- ajax/open/connect.php
+++ ajax/open/connect.php
@@ -2,12 +2,16 @@
 require_once("../../internal/logic/db.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	if(!isset($_POST['data'])) return;
 	$stmtMessage = $db->prepare("INSERT INTO meg2.data (data, channel) VALUES (:data, :channel); ");
     $stmtMessage->execute(array('channel' => isset($_POST['channel']) ? $_POST['channel'] : 0, 'data' => json_encode($_POST['data'])));
 } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 	$stmtMessage = $db->prepare("SELECT * FROM meg2.data WHERE id > :last AND channel = :channel; ");
     $stmtMessage->execute(array('channel' => isset($_POST['channel']) ? $_POST['channel'] : 0, 'last' => isset($_POST['last']) ? $_POST['last'] : -1));
-    $data = (array)$stmtMessage->fetchAll();
+    $data = array();
+    while($row = $stmtMessage->fetchObject()){
+		$data[] = json_decode(((array)$row)['data']);
+	}
     echo json_encode($data);
 }
 
--- api/script.js
+++ api/script.js
@@ -1,1 +1,59 @@
+window.listeners = {};
+window.lasts = {};
 
+window.chatserver = {
+    onNewMessage: function(then, channel = 0){
+		if(!(channel in listeners)) listeners[channel] = [];
+	    listeners[channel].push(then);
+	},
+	send: function(data, channel = 0){
+		if(!data) return;
+		
+		
+	}
+};
+
+function load_channel(channel){
+	return new Promise(function(resolve, reject){
+		let xhr = new XMLHttpRequest();
+		xhr.open("POST", "https://meg-chat.de/ajax/open/connect.php");
+		xhr.setRequestHeader("Accept", "application/json");
+		xhr.setRequestHeader("Content-Type", "application/json");
+		
+		xhr.onreadystatechange = function () {
+			resolve();
+		    if (xhr.readyState === 4) {
+				var data = JSON.parse(xhr.responseText);
+				listeners[channel].forEach(function(l){
+					try {
+					    data.forEach(function(m){
+							try {
+						        l(m);
+						    } catch(e){
+							    console.log(e);	
+							}
+						});
+					} catch(e){
+					    console.log(e);	
+					}
+				});
+				
+		    }
+		};
+		
+		let data = {
+		  channel: channel,
+		  last: lasts[channel] || -1;
+		};
+		
+		xhr.send(JSON.stringify(data));
+	});
+}
+async function load(){
+	for(var i = 0; i < Object.keys(listeners).length; i++){
+		var channel = Object.keys(listeners)[i];
+	    await load_channel(channel)
+	});
+	setTimeout(load, 100);
+}
+load();
