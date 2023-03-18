b24f486498b9144f5d8881f0dbd448eb66ec0e69
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-18 22:14:18 +0100

    Update connect.php

--- ajax/open/connect.php
+++ ajax/open/connect.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmtMessage->execute(array('channel' => isset($_POST['channel']) ? $_POST['channel'] : 0, 'data' => json_encode($_POST['data'])));
 } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 	$stmtMessage = $db->prepare("SELECT * FROM meg2.data WHERE id > :last AND channel = :channel; ");
-    $stmtMessage->execute(array('channel' => isset($_POST['channel']) ? $_POST['channel'] : 0, 'last' => json_encode($_POST['data'])));
+    $stmtMessage->execute(array('channel' => isset($_POST['channel']) ? $_POST['channel'] : 0, 'last' => isset($_POST['last']) ? $_POST['last'] : -1);
     $data = (array)$stmtMessage->fetchAll();
     echo json_encode($data);
 }
