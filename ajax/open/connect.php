e32bc9bb60dc247f16a766ccc4f536ec8a38fadf
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-19 08:36:25 +0100

    Update connect.php

--- ajax/open/connect.php
+++ ajax/open/connect.php
@@ -1,5 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 require_once("../../internal/logic/db.php");
 
