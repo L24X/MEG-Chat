03c060305ee18167a5a16f3a69f1d346d4870e2c
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-21 18:56:48 +0100

    Chat Upload

--- ajax/upload_file.php
+++ ajax/upload_file.php
@@ -6,14 +6,18 @@ if(!isset($_SESSION['pupil'])){
 	return exit();
 }
 
+$offset = intval($_POST['offset']);
+if($offset == 0){
+    $_SESSION[basename($_FILES['file']['name'])] = rand(100000,100000000);
+}
+
 if(!isset($_SESSION[basename($_FILES['file']['name'])])) $_SESSION[basename($_FILES['file']['name'])] = rand(100000,100000000);
 $targetDir = '/media/hdd1/';
 $targetFile = $targetDir . $_SESSION['pupil'] . "_" . $_SESSION[basename($_FILES['file']['name'])] . "_" . basename($_FILES['file']['name']);
 
 if (isset($_FILES['file']) && isset($_POST['offset']) && isset($_POST['filesize'])) {
   echo json_encode($_FILES['file']);
-  
-  $offset = intval($_POST['offset']);
+
   $offsetFile = file_exists($targetFile) ? filesize($targetFile) : 0;
   if($offset != $offsetFile){
       echo "Fehler!";
@@ -23,12 +27,11 @@ if (isset($_FILES['file']) && isset($_POST['offset']) && isset($_POST['filesize'
 
   $fileSize = intval($_POST['filesize']);
 
-  $out = fopen($targetFile, 'ab');
-  $in = fopen($_FILES['file']['tmp_name'], 'rb');
-  fseek($in, $offset);
-  stream_copy_to_stream($in, $out);
-  fclose($in);
-  fclose($out);
+  $fileData = file_get_contents($_FILES['file']['tmp_name']);
+  $fileHandle = fopen($targetFile, 'r+');
+  fseek($fileHandle, $offset);
+  fwrite($fileHandle, $fileData);
+  fclose($fileHandle);
 
   if ($offset + $_FILES['file']['size'] >= $fileSize) {
     echo 'Upload complete!';
