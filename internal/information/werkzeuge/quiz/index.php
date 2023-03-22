e8ee89375c699c57e19d78f931ae907ca778c2c1
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-22 20:20:56 +0100

    Werkzeuge

--- internal/information/werkzeuge/quiz/index.php
+++ internal/information/werkzeuge/quiz/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once("../logic/db.php");
+require_once("../../../logic/db.php");
 
 if(isset($_SESSION['pupil'])){
 	$stmtCheck = $db->prepare("SELECT * FROM ".DBTBL.".pupils WHERE id = :id;");
@@ -16,10 +16,10 @@ if(isset($_SESSION['pupil'])){
         <title>MEG Chat | Werkzeuge | Quiz</title>
         <meta name="description" content="Erstellen Sie ganz einfach Quize mit dem MEG Quiz Ersteller. Kostenlos einfach online im Browser benutzbar. Erstellen Sie ein Quiz und spielen es mit der ganzen Klasse.">
         <meta name="keywords" lang="de" content="meg, max ernst gymnasium, quiz, erstellen, kostenlos, online, kahoot, fragen, spielen">
-        <?php require('../middleware/head.php'); ?>
+        <?php require('../../../middleware/head.php'); ?>
     </head>
     <body>
-		<?php require('../middleware/navbar.php'); ?>
+		<?php require('../../../middleware/navbar.php'); ?>
 		<div id="site_container">
 		    <div style="margin-top: 80px; ">
 		        <h1>Quiz Erstellen</h1>
