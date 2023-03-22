f2767cafbcd2123f05e772adb60f7d00715effcd
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-22 19:47:56 +0100

    Werkzeuge

---
+++ internal/information/werkzeuge.php
@@ -0,0 +1,30 @@
+<?php
+<?php
+require_once("../logic/db.php");
+
+if(isset($_SESSION['pupil'])){
+	$stmtCheck = $db->prepare("SELECT * FROM ".DBTBL.".pupils WHERE id = :id;");
+	$stmtCheck->execute(array('id' => $_SESSION['pupil']));
+	$pupil_data = (array)$stmtCheck->fetchObject();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MEG Chat | Schueler</title>
+        <meta name="description" content="<?php echo htmlspecialchars($blog_data['text']); ?>">
+        <meta name="keywords" lang="de" content="meg, max ernst gymnasium, schueler, suchen, liste, mitglieder">
+        <?php require('../middleware/head.php'); ?>
+    </head>
+    <body>
+		<?php require('../middleware/navbar.php'); ?>
+		<div id="site_container">
+		    <ul>
+		        <li><a href="/werkzeuge/quiz/">Quiz erstellen</a></li>
+		    </ul>
+		</div>
+	</body>
+</html>
